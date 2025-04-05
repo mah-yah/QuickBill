d2f5dd990cb4e772417ee71bfea1ebe1eac31f23
Mahsa Yahyazadeh <MYahyazadeh@ifschain.com>
2025-04-05 05:07:58 +0330

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-43A5WTQQ.mjs";import"./chunk-OYDMQNJQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7HNI3WSC.mjs";import"./chunk-OYDMQNJQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
